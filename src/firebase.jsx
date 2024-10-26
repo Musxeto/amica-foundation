bceb9d79f0dad35ea465e4a41326503c954e4864
musxeto <mustafamalikawan786@gmail.com>
2024-10-26 11:16:18 +0500

    inital commits

---
+++ src/firebase.jsx
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDgtBVh_H5WMME7uMzciUoIO2qS4v2rMUU",
+  authDomain: "amico-a0a2e.firebaseapp.com",
+  projectId: "amico-a0a2e",
+  storageBucket: "amico-a0a2e.appspot.com",
+  messagingSenderId: "392600188004",
+  appId: "1:392600188004:web:15492736246b44e975db71"
+};
+
+const app = initializeApp(firebaseConfig);
